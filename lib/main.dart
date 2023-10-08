4f1b7056e75f066577121a78f3a25c6cccaf600e
Ravi teja <52047282+ravitejanalla17@users.noreply.github.com>
2023-10-08 23:11:45 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,17 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'package:recipe_2/news_list.dart';
-import 'bloc/news_bloc.dart';
+import 'package:news_app/country_bloc/bloc/country_bloc.dart';
+import 'package:flutter/material.dart';
+import 'country_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: BlocProvider(
-        create: (context) => NewsBloc(),
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('News App'),
-          ),
-          body: NewsList(),
-        ),
-      ),
+      home: CountryScreen(),
     );
   }
 }
